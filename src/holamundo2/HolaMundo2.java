c48ad75a969d4594c87e679896eeb060bafeaee5
Jose Alvarez Barciela <jalvarezbarciela@nautilus6.danielcastelao.org>
2019-10-16 13:46:09 +0200

    primer commit

---
+++ src/holamundo2/HolaMundo2.java
@@ -0,0 +1,14 @@
+
+package holamundo2;
+
+/**
+ *
+ * @author jalvarezbarciela
+ */
+public class HolaMundo2 {
+
+    public static void main(String[] args) {
+       System.out.println("Hola Mundo");
+    }
+    
+}
