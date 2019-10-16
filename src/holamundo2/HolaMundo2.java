460de98fb8bb617717d5437bcd1a4f6839139ff7
Jose Alvarez Barciela <jalvarezbarciela@nautilus6.danielcastelao.org>
2019-10-16 13:47:36 +0200

    segundo commit

--- src/holamundo2/HolaMundo2.java
+++ src/holamundo2/HolaMundo2.java
@@ -8,7 +8,7 @@ package holamundo2;
 public class HolaMundo2 {
 
     public static void main(String[] args) {
-       System.out.println("Hola Mundo");
+       System.out.println("Hola Vigo");
     }
     
 }
